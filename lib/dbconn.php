5221fa053d53c95d7a29a3ea0b05adfc81407699
eccynd <eccynd@naver.com>
2020-11-09 15:34:37 +0900

    dbconn.php 파일 수정

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -1,5 +1,5 @@
 <?
-    $connect=mysql_connect( "localhost", "eccynd", "hjp1630319!") or  
+    $connect=mysql_connect( "localhost", "", "") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
     mysql_select_db("eccynd",$connect);
